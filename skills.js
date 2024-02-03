2bd5899c5868293da0bb8456d8ec06d84959957a
Pavel Filingeri <124843496+pavelux00x@users.noreply.github.com>
2024-02-03 11:05:41 +0000

    Calcnumb2

--- skills.js
+++ skills.js
@@ -1,4 +1,6 @@
 function calculateNumbers(var1,var2){
     console.log("Calculating numbers");
+    //add a print line
+    console.log("Adding numbers: " + var1 + " + " + var2);
     return var1 + var2;
 }
